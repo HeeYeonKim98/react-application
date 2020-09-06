c7738a55e47485e6b4d78599e1bb124c0ff8ce52
HeeYeonKim98 <wow9896@naver.com>
2020-09-07 04:47:21 +0900

    update fetch

--- src/App.js
+++ src/App.js
@@ -1,10 +1,37 @@
 import React, { Component } from "react";
 
 export default class App extends Component {
+  state = {
+    infos: [],
+  };
+
+  async componentDidMount() {
+    const res = await fetch("http://127.0.0.1:8000/api/");
+    const infos = await res.json();
+    console.log(infos);
+
+    this.setState({ infos });
+  }
+
   render() {
+    const { infos } = this.state;
+    console.log(infos);
+
     return (
       <div>
-        <div>rdproject-react</div>
+        <h1>react-django 연동</h1>
+        <div>
+          {infos.map((i) => {
+            return (
+              <div>
+                <div>{i.key}</div>
+                <div>{i.id}</div>
+                <div>{i.title}</div>
+                <div>{i.content}</div>
+              </div>
+            );
+          })}
+        </div>
       </div>
     );
   }
